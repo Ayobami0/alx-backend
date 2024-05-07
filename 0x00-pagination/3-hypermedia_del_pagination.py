436f1562928118d2bdf8d595c963bccd11ac6ccc
Ayobami <oludemiayobami@gmail.com>
2024-05-07 09:41:04 +0100

    style: autopep8 fix

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -8,8 +8,8 @@ from typing import Dict, List, Union
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
+
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
@@ -17,8 +17,7 @@ class Server:
         self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cached dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -28,22 +27,20 @@ class Server:
         return self.__dataset
 
     def indexed_dataset(self) -> Dict[int, List]:
-        """Dataset indexed by sorting position, starting at 0
-        """
+        """Dataset indexed by sorting position, starting at 0"""
         if self.__indexed_dataset is None:
             dataset = self.dataset()
             # truncated_dataset = dataset[:1000]
-            self.__indexed_dataset = {
-                i: dataset[i] for i in range(len(dataset))
-            }
+            self.__indexed_dataset = {i: dataset[i]
+                                      for i in range(len(dataset))}
         return self.__indexed_dataset
 
     def get_hyper_index(
-            self,
-            index: Union[int, None] = None,
-            page_size: int = 10,) -> Dict:
-        """Gets the index of a truncated data
-        """
+        self,
+        index: Union[int, None] = None,
+        page_size: int = 10,
+    ) -> Dict:
+        """Gets the index of a truncated data"""
         if index is not None:
             assert index >= 0 and index < len(self.indexed_dataset())
         else:
